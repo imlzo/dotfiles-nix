d7895d46749f03a2a94066dceb1ae5334bb2e47c
imlzo <56844000+imlzo@users.noreply.github.com>
2022-10-05 09:02:50 -0700

    feat: Add alacritty configuration

--- nixpkgs/linux.nix
+++ nixpkgs/linux.nix
@@ -1,5 +1,18 @@
 { config, lib, pkgs, ...}:
-{
+let
+  nixgl = import <nixgl> {};
+  nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+       wrapped_bin=$out/bin/$(basename $bin)
+       echo "exec ${lib.getExe nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
+       chmod +x $wrapped_bin
+      done
+    '';
+in {
   targets.genericLinux.enable = true;
 
   home.packages = with pkgs; [
@@ -8,5 +21,11 @@
     xclip
   ];
 
+  # alacritty
+  # xdg.configFile.alacritty.source = ~/dotfiles/alacritty;
+  programs.alacritty = {
+    enable = true;
+    package = nixGLWrap pkgs.alacritty;
+  };
 
 }
