5f668a015045ee948a98babd41d6ae61420b2057
imlzo <56844000+imlzo@users.noreply.github.com>
2022-10-16 11:00:00 -0700

    make: Update nvim.mk to run PackerSync

--- makefiles/bootstrap.mk
+++
@@ -1,6 +0,0 @@
-# Makefile - Bootstrap
-
-.PHONY: bootstrap_nvim
-bootstrap_nvim:																														## bootstrap packer
-	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
--- makefiles/nvim.mk
+++ makefiles/nvim.mk
@@ -1,6 +1,9 @@
-# Makefile - Nvim
+#
+# nvim.mk - Makefile for nvim
+#
 
-.PHONY: nvim_bootstrap
-nvim_bootstrap:							## bootstrap nvim
+.PHONY: nvim_setup
+nvim_setup:							## setup nvim
 	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+	nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' 2>/dev/null
 
