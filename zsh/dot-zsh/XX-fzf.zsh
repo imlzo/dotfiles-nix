0f8b1c66df03bd379042c479d89153cf6c4a459c
imlzo <56844000+imlzo@users.noreply.github.com>
2022-10-16 08:16:34 -0700

    fzf: Add keybindings and completion

--- zsh/dot-zsh/XX-fzf.zsh
+++ zsh/dot-zsh/XX-fzf.zsh
@@ -2,6 +2,11 @@
 # XX-fzf.zsh    - Configuration for FZF
 #
 
+if [ -n "${commands[fzf-share]}" ]; then
+  source "$(fzf-share)/key-bindings.zsh"
+  source "$(fzf-share)/completion.zsh"
+fi
+
 # Use fd instead of find. Snappiness = happiness
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 _fzf_compgen_path() {
