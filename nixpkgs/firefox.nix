f3bb9f6d800f3d0d22458e4e7026b81b5d0fdf8f
imlzo <56844000+imlzo@users.noreply.github.com>
2022-10-18 09:12:33 -0700

    firefox: Add tree-style-tab and userChrome

--- nixpkgs/firefox.nix
+++ nixpkgs/firefox.nix
@@ -8,6 +8,7 @@ in {
       vimium
       onepassword-password-manager
       rust-search-extension
+      tree-style-tab
     ];
     profiles = {
       default = {
@@ -16,7 +17,35 @@ in {
           "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
           "signon.rememberSignons" = false;
           "browser.tabs.unloadOnLowMemory" = true;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         };
+
+        userChrome = "
+          #tabbrowser-tabs {
+            visibility: collapse !important;
+          }
+
+          #titlebar {
+            appearance: none !important;
+            height: 0px;
+          }
+
+          #titlebar > #toolbar-menubar {
+            margin-top: 0px;
+          }
+
+          #TabsToolbar {
+            min-width: 0 !important;
+            min-height: 0 !important;
+          }
+
+          #TabsToolbar > .titlebar-buttonbox-container {
+            display: block;
+            position: absolute;
+            top: 12px;
+            left: 0px;
+          }
+        ";
       };
 
     };
