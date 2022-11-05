50f855ffad1747fe491b9d7fe11971305df863ec
imlzo <56844000+imlzo@users.noreply.github.com>
2022-11-05 05:56:43 -0700

    nvim: Add yank to * keymaps

--- nvim/lua/yank.lua
+++ nvim/lua/yank.lua
@@ -5,7 +5,9 @@ local cmd = vim.cmd
 local map = vim.api.nvim_set_keymap
 
 -- Leader+Y to system clipboard
+map('n', '<leader>y', '"*yy', { noremap = true })
 map('v', '<leader>y', '"*y', { noremap = true })
+map('n', '<leader>p', '"*p', { noremap = true })
 
 -- Highlight on yank
 cmd [[
