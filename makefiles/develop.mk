9d6ac7358d37234e147e0dd07028ece1a2b6208a
imlzo <56844000+imlzo@users.noreply.github.com>
2022-12-10 16:29:30 -0800

    make: Add back develop_web

--- makefiles/develop.mk
+++ makefiles/develop.mk
@@ -1,8 +1,17 @@
 #
 # Makefile - Development Setups
+#
 
-.PHONY: nix_node2nix
-nix_node2nix:																															## Run node2nix to update node-packages.nix
-	(cd $(HOME)/dotfiles/nixpkgs/node/; \
-		nix-shell -p nodePackages.node2nix --command "node2nix -i node-packages.json")
+SHELL := /bin/bash
+
+.PHONY: develop_web
+develop_web:																															## Install web dependencies
+	command -v nvm >/dev/null || curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+	export NVM_DIR="$(HOME)/.nvm"
+	[ -s "$(NVM_DIR)/nvm.sh" ] && \. "$(NVM_DIR)/nvm.sh"  # This loads nvm
+	[ -s "$(NVM_DIR)/bash_completion" ] && \. "$(NVM_DIR)/bash_completion"  # This loads nvm bash_completion
+	nvm install node
+	npm install --global typescript-language-server typescript
+	npm install --global vscode-langservers-extracted
+	npm install --global yarn
 
